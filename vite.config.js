48fe2b55721b908427dba0b2bb7bd06807b91fd4
Paradoxai77 <pratiknerpagar2@gmail.com>
2026-04-19 17:01:08 +0530

    Auto GH-Pages Fix

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/Traffic-System/',
+  base: '/Traffic-System/dist/',
   plugins: [react()],
 })
